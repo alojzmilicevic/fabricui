74a295b5e8f56b7f0c2be637fe944bdc6dd3de87
Alojz Milicevic <alojz.milicevic@gmail.com>
2023-02-04 01:21:56 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+import { DragableListExample } from './components/DragableList/example/DragableListExample';
+
+const MainWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    background-color: white;
+    align-items: center;
+`;
+
+function App() {
+    return (
+        <MainWrapper>
+        </MainWrapper>
+    );
+}
+
+export default App;
